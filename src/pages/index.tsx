983a014a29b3f7902b8eb343053381c844f779f5
Wajahat Ali Khan <assim345678@gmail.com>
2024-11-25 15:40:19 +0500

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-black via-gray-800 to-black text-white">
+      <div className="text-center p-8 bg-opacity-50 bg-gray-900 rounded-lg shadow-lg">
+        <h1 className="text-4xl font-bold text-yellow-400 mb-4">Hello World</h1>
+        <p className="text-lg text-white mb-2">Quarter 2 Governor Sindh IT Course</p> 
+        <p className="text-lg text-sky-300">Wajahat Ali</p> 
+      </div>
+    </div>
+  );
+}
